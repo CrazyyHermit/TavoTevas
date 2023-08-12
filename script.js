07e4e9ffbbbd51eb3bccf92a163205ae86fa3673
CrazyyHermit <airisgrow.man@gmail.com>
2023-08-12 21:21:56 +0300

    changed it so it works with github

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function doTheFunny(){
-    document.body.innerHTML = '';
-    var audio = new Audio('sounds/cave1.mp3');
-    var body = document.getElementById('body');
-    var image = document.createElement('p');
-    image.className = "tevas";
-    body.appendChild(image)
-    audio.play()
-};
+
