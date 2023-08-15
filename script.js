61df3c1adac3f729f1e3741c5d5d8a445e656b8e
BuildTools <unconfigured@null.spigotmc.org>
2023-08-15 16:23:00 +0300

    Added obama easter egg, fixed the easter egg check

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var images = ["cat.png", "tavotevas.webp", "sad.jpg"];
-var sounds = ["cave1.mp3", "fart.mp3"]
+var images = ["cat.png", "tavotevas.webp", "sad.jpg", "cursedObama.gif"];
+var sounds = ["cave1.mp3", "fart.mp3", "knock.mp3"]
 var keyInput = "";
 var imageSelect = 1
 var soundSelect = 0
@@ -7,11 +7,12 @@ var wordsCheck;
 
 var AudioPlaying = false;
 
-function AudioStopped(){;
+function AudioStopped(close){; //Doesn't allow user to press on the screen until the audio finishes playing
     AudioPlaying = false;
+    document.body.innerHTML = '';
 };
 
-function doTheFunny(){
+function doTheFunny(){ // on click command
     if (!AudioPlaying) {
         switch (keyInput){
             case "CAT":
@@ -21,6 +22,10 @@ function doTheFunny(){
             case "SAD":
                 imageSelect = 2;
                 soundSelect = 1;
+                break;
+            case "OBAMA":
+                imageSelect = 3;
+                soundSelect = 2;
                 break;
             default:
                 imageSelect = 1;
@@ -53,41 +58,46 @@ window.addEventListener("keypress", easteregg);
 
 var LeftOverWords;
 
-function easteregg(key){
+function easteregg(key){ //Checks for key input
     var input = key.code.replace("Key", "");
-    var wordFound = false;
-    console.log("input = "+input)
+    var letterFound = false;
+    console.log("input = "+input);
 
-    var words = ["CAT", "SAD"];
+    const words = ["CAT", "SAD", "OBAMA"];
     if (keyInput.length == 0){
         wordsCheck = words;
     };
-    console.log(wordsCheck);
 
-    LeftOverWords = wordsCheck
-    keyInputBeforeCheck = keyInput
+    if (wordsCheck.length == 1 && wordsCheck[0] == keyInput){ // this is here so when you type in a code for easter egg you can then immediately type in another code without having to fail first by typing in a letter that doesn't match wordsCheck[0]
+        keyInput = "";
+        wordsCheck = words;
+        console.log("RESET ACTIVATED")
+    };
+
+    keyInputBeforeCheck = keyInput;
+    var removedWords = [];
 
     for (let word in wordsCheck) {
-        console.log(`Checking `+wordsCheck[word])
-
         if (wordsCheck[word][keyInputBeforeCheck.length] == input){
-            console.log(`added `+input);
+            if (letterFound) {continue;};
             keyInput += input;
-            wordFound = true;
+            letterFound = true;
             continue;
+            console.log(wordsCheck[word]+` stays`);
         }
         else {
-            console.log(wordsCheck[word]+` is not the word, removing.`)
-            delete LeftOverWords[word];
+            console.log(wordsCheck[word]+` REMOVED`);
+            removedWords.push(wordsCheck[word]);
             continue;
         };
     };
-
-
-    if (!wordFound){
+    if (!letterFound){
         keyInput = "";
-        console.log("Word not found, clearing word cache.")
-        console.log(wordsCheck);
     };
-    wordsCheck = LeftOverWords;
+    //removing words that have been detected to not have checked letters
+    wordsCheck = wordsCheck.filter((el)=> !removedWords.includes(el)); 
+
+    console.log('Words in the list: '+wordsCheck.length)
 };
+
+
